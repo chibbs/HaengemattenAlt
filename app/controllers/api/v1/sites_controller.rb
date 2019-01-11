5568b4cc524b33a93a24070db9891e9309efee50
Jannis Jahr <>
2019-01-11 23:18:11 +0100

    extended api and loading messages in ajax protocol (todo: put into messages box)

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -1,57 +1,47 @@
 class Api::V1::SitesController < Api::V1::BaseController
+  before_action only: [:show, :edit, :update, :destroy]
   # todo: put into serializer
   def index
     serialized = []
     Site.all.each do |site|
-      attribute = {
-          name: site.name,
-          description: site.description,
-          user: {
-              first: site.user.first_name,
-              last: site.user.last_name,
-          },
-          position: {
-              lon: site.longitude.to_f,
-              lat: site.latitude.to_f,
-          },
-          belongs_to_user: site.user == current_user,
-          review_count: site.reviews.size,
-          detail_page: site_path(site)
-      }
-      if site.user == current_user
-        attribute.merge!(delete: "TODO:IMPLEMENT",
-                         edit: edit_site_path(site))
-      end
+      attribute = format_site(site)
       serialized.push(attribute)
     end
     render(json:serialized)
   end
 
-
-  def create
-    @site = Site.new(site_params)
-    @site.user_id = current_user.id
-
-    if params[:site][:size_ids].present?
-      sizesgroup = Size.find params[:site][:size_ids]
-      @site.sizes = sizesgroup
-    end
-
-    if @site.save
-      redirect_to sites_url, notice: 'Hängematte wurde angelegt.'
+  def show
+    @site = Site.find_by id: params[:id]
+    if @site == nil
+      render(json:{"error": "Key not found"})
     else
-      redirect_to sites_url, notice: 'Hängematte konnte nicht angelegt werden, weil Attribute fehlen.'
+      reviews = Review.where(site_id: @site.id)
+      render(json:format_site(@site).merge!("reviews": reviews))
     end
   end
 
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_site
-    @site = Site.joins(:sizes).includes([:sizes, :reviews]).find(params[:id])
+  def format_site(site)
+    attribute = {
+        name: site.name,
+        id: site.id,
+        description: site.description,
+        user: {
+            first: site.user.first_name,
+            last: site.user.last_name,
+        },
+        position: {
+            lon: site.longitude.to_f,
+            lat: site.latitude.to_f,
+        },
+        belongs_to_user: site.user == current_user,
+        review_count: site.reviews.size,
+        detail_page: api_v1_site_path(site),
+    }
+    if site.user == current_user
+      attribute.merge!(delete: "TODO:IMPLEMENT",
+                       edit: edit_site_path(site))
+    end
+    attribute
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def site_params
-    params.require(:site).permit(:name, :description, :longitude, :latitude, :images, images_attachments_attributes: [:id, :_destroy])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sites, only: [:index, :create, :show, :update, :destroy]
-      resources :microposts, only: [:index, :create, :show, :update, :destroy]
+      resources :reviews, only: [:index, :create, :show, :update, :destroy]
     end
   end
   
