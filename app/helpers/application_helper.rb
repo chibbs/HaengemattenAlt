167cb42e150c75fba592529f927fc692e88aea0a
Laura Woelbeling <s0548106@htw-berlin.de>
2019-01-22 04:09:30 +0100

    cross hover and click (show) working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	# build an empty ajax link, which we can use with jquery.wrap()
+	def empty_ajax_link_to(href)
+		"<a href='#{href}' data-remote='true'></a>".html_safe
+  end
 end
