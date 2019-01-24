53fff434d6b576ff556ada4527b21a5e014fa688
Laura Woelbeling <s0548106@htw-berlin.de>
2019-01-24 11:09:28 +0100

    very basic admin page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  # GET /home/admin
+  def admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  get 'admin', to: 'home#admin', as: 'admin'
 end
